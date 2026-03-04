028037c0ed97dfee700aa74926197fd516cfbf9c
WilhelmLiljegren <wilhelm.liljegren@elev.ga.ntig.se>
2026-03-04 16:07:23 +0100

    v.10
    BIG DATABAS

--- app.rb
+++ app.rb
@@ -3,3 +3,21 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+
+enable :session
+
+get('/login') do
+    username = params ["username"]
+    password = params ["password"]
+    password_confirmation = params ["password_confirmation"]
+    result = db.execute('SELECT * FROM user WHERE username = ?', [username])
+    
+    if result.empty?
+        if password == password_confirmation
+            password_digest = BCrypt::Password.create(password)
+            p password_digest
+            db.execute("INSERT INTO  user(username, pwd) VALUES (?,?)", [username, password_digest])
+        else 
+            set_error ... -->
+    slim(:login)
+    end
--- db/seeder.rb
+++ db/seeder.rb
@@ -4,7 +4,7 @@ db = SQLite3::Database.new("databas.db")
 
 
 def seed!(db)
-  puts "Using db file: db/todos.db"
+  puts "Using db file: db/wspxxv.db"
   puts "🧹 Dropping old tables..."
   drop_tables(db)
   puts "🧱 Creating tables..."
@@ -19,19 +19,43 @@ def drop_tables(db)
 end
 
 def create_tables(db)
-  db.execute('CREATE TABLE exempel (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL, 
-              description TEXT,
-              state BOOLEAN)')
+  db.execute('CREATE TABLE user (
+              user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+              username TEXT NOT NULL, 
+              pwd TEXT)')
+
+  db.execute('CREATE TABLE story (
+  headline TEXT NOT NULL, content TEXT NOT NULL, 
+  story_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  user_id INT, FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE)')
+
+  db.execute ('CREATE TABLE votes(
+  user_id INT,
+  story_id INT,
+  PRIMARY KEY (user_id, story_id)
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (story_id) REFERENCES story(story_id)
+  ON DELETE CASCADE
+  upvote BOOLEAN,
+  downvote BOOLEAN)') #Skapa en knapp
+
+  db.execute ('CREATE TABLE comments(
+  user_id INT,
+  story_id INT,
+  PRIMARY KEY (user_id, story_id)
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (story_id) REFERENCES story(story_id)
+  ON DELETE CASCADE
+  comment TEXT)')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp mjölk", "3 liter mellanmjölk, eko",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp julgran", "En rödgran",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten",false)')
+  db.execute('INSERT INTO user (username, pwd,) VALUES ("User_X", "pwd X")')
+  db.execute('INSERT INTO user (username, pwd,) VALUES ("User_Y", "pwd Y")')
+  db.execute('INSERT INTO user (username, pwd,) VALUES ("User_Z", "pwd Z")')
 end
-
 
 seed!(db)
 
