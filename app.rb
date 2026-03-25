ac65c8edde808879f5c3968d3541b28af813552f
WilhelmLiljegren <wilhelm.liljegren@elev.ga.ntig.se>
2026-03-25 16:21:43 +0100

    v.13

--- app.rb
+++ app.rb
@@ -6,31 +6,52 @@ require 'bcrypt'
 
 enable :session
 
-get('/login') do
-    username = params ["username"]
-    password = params ["password"]
-    password_confirmation = params ["password_confirmation"]
-    result = db.execute('SELECT * FROM user WHERE username = ?', [username])
+    get('/') do
+        
     
-    if result.empty?
-        if password == password_confirmation
-            password_digest = BCrypt::Password.create(password)
-            p password_digest
-            db.execute("INSERT INTO  user(username, pwd) VALUES (?,?)", [username, password_digest])
-            redirect(:layout)
-        else 
-            @error = "Invalid username or password"
+    end
+
+    get('/welcome') do
+
+
+    end
+
+
+    get('/login') do
+        username = params ["username"]
+        password = params ["password"]
+        password_confirmation = params ["password_confirmation"]
+        result = db.execute('SELECT * FROM user WHERE username = ?', [username])
+        
+        if result.empty?
+            if password == password_confirmation
+                password_digest = BCrypt::Password.create(password)
+                p password_digest
+                db.execute("INSERT INTO  user(username, pwd) VALUES (?,?)", [username, password_digest])
+                redirect(:layout)
+            else 
+                @error = "Invalid username or password"
+            end
         end
-    end
-    slim(:login)
+        slim(:login)
     end
 
-get('/stories/') do
-    query = params [:q]
-    5
-end
+    get('/new_story') do
+    slim(:new_story)
+    end
 
-get('/clear_session')
-    session.clear
-    slim(:login)
-end
+    post('/story') do
+        new_story = params[:new_story]
+        headline = params[:headline]
+        content = params[:content]
+        story_id = params[:story_id].to_i
+        user_id = params[:user_id].to_i
+            db = SQqlite3::Database.new('db/story.db') #???
+            db.execute("INSERT INTO story (headline, content, story_id, user_id) VALUES (?,?,?,?)")
+            redirect(:main)
+    end
+
+    get('/clear_session') do
+        session.clear
+        slim(:login)
+    end
